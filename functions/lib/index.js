e45c8f408f8fd4f6eb5f34551416971637f76b80
Mochamad Arifin <klampok.child@gmail.com>
2018-06-24 06:21:06 +0700

    First Test Ready

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,15 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-const paypal = require('paypal-rest-sdk');
 // firebase-admin SDK init
 const admin = require('firebase-admin');
 admin.initializeApp();
-
-exports.pay = functions.https.onRequest((request, response) => {
-    response.send("Payment response!");
+// Configure your environment
+console.log('Config:', functions.config());
+exports.paypal_return = functions.https.onRequest((request, response) => {
+    let newData = admin.database().ref('/paypal_logs').push();
+    newData.set({
+        params: request.params,
+        raw: request.body
+    });
+    response.send("Payment response saved on realtime DB: " + newData.key);
 });
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,17 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-
-const paypal = require('paypal-rest-sdk');
 // firebase-admin SDK init
 const admin = require('firebase-admin');
 admin.initializeApp();
+
 // Configure your environment
 console.log('Config:', functions.config());
-paypal.configure({
-  mode: 'sandbox', // sandbox or live
-  client_id: functions.config().paypal.sandbox.client_id,
-  client_secret: functions.config().paypal.sandbox.client_secret
-});
 
-export const pay = functions.https.onRequest((request, response) => {
-    response.send("Payment response!");
+export const paypal_return = functions.https.onRequest((request, response) => {
+  let newData = admin.database().ref('/paypal_logs').push();
+  newData.set({
+      params: request.params,
+      raw: request.body
+  })
+  response.send("Payment response saved on realtime DB: " + newData.key);
 });
